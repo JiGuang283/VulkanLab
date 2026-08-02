01960a747d6832c07d8e4514d278001968f67353
Ji_Guang283 <w2831867639@gmail.com>
2026-08-02 14:12:32 +0800

    build: add editor-only ImGuizmo dependency

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -227,4 +227,15 @@ if(VKL_ENABLE_EDITOR_UI)
         "${VKL_IMGUI_DIR}/backends"
     )
     target_link_libraries(vkl_imgui PUBLIC vkl_glfw Vulkan::Vulkan)
+
+    set(VKL_IMGUIZMO_DIR "${PROJECT_SOURCE_DIR}/external/ImGuizmo")
+    add_library(vkl_imguizmo STATIC
+        "${VKL_IMGUIZMO_DIR}/src/ImGuizmo.cpp"
+    )
+    add_library(VulkanLab::ImGuizmo ALIAS vkl_imguizmo)
+    target_compile_features(vkl_imguizmo PRIVATE cxx_std_17)
+    target_include_directories(vkl_imguizmo SYSTEM PUBLIC
+        "${VKL_IMGUIZMO_DIR}/src"
+    )
+    target_link_libraries(vkl_imguizmo PUBLIC vkl_imgui)
 endif()
